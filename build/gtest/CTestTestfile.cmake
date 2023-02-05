f1c22c1837c8a5e9876bf603f33fa60e822090ba
fedzzzz <fedi.adami@gmail.com>
2023-02-05 17:15:03 +0100

    cmake fix

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/fed/ros_ws/src/locosim/robot_control/lab_exercises/lab_palopoli/cpp/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
